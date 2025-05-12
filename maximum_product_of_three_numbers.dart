92b80c17d8596ec77a4c6271b8060edad8d39880
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-05-12 19:42:13 +0600

    maximum product of three numbers leetcode

--- maximum_product_of_three_numbers.dart
+++ maximum_product_of_three_numbers.dart
@@ -13,10 +13,13 @@ void main() {
 
 int maximumProduct(List<int> nums) {
   var result = 0;
+
   nums.sort();
-  int max_1 =
-      nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+
+  int max_1 = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
   int max_2 = nums[0] * nums[1] * nums[nums.length - 1];
+
   result = max(max_1, max_2);
+  
   return result;
 }
