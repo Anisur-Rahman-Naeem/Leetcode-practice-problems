8783d262f638b7d4aaa4c2e265cd6ef589aa1d05
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-05-17 14:22:03 +0600

    from leetcode

--- intersection_of_two_arrays.dart
+++ intersection_of_two_arrays.dart
@@ -2,8 +2,10 @@ class Solution {
   List<int> intersection(List<int> nums1, List<int> nums2) {
     Set<int> unique_elements = {};
 
+    Set<int>set2 = nums2.toSet();
+
     for(var element in nums1){
-        if(nums2.contains(element)){
+        if(set2.contains(element)){
             unique_elements.add(element);
         }
     }
