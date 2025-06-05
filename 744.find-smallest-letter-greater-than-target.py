b913758e9e696df828bc4130c1ddb0c3e39fb75d
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-06-06 02:30:57 +0600

    from leetcode using binary search

---
+++ 744.find-smallest-letter-greater-than-target.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=744 lang=python
+#
+# [744] Find Smallest Letter Greater Than Target
+#
+
+# @lc code=start
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        left = 0
+        right = len(letters) -1
+
+        while left <= right:
+            mid = (left + right)//2
+            if letters[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return letters[left] if left < len(letters) else letters[0]
+        
+# @lc code=end
+
