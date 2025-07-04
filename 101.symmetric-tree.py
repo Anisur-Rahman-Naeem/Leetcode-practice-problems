8cf979702dcf4950622f6a5101b01146226c4ee9
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-07-04 10:13:28 +0600

    from leetcode using binary tree

---
+++ 101.symmetric-tree.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=101 lang=python
+#
+# [101] Symmetric Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not root:
+            return True
+        
+        def isSymmetric(t1, t2):
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            return (t1.val == t2.val and 
+            isSymmetric(t1.left, t2.right) and
+            isSymmetric(t1.right, t2.left))
+        
+        return isSymmetric(root.left, root.right)
+
+        _
+# @lc code=end
+
