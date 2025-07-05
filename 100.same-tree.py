d765570be928d7bd3e1370b68b96df08a0559c1e
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-07-05 10:22:13 +0600

    using binary tree and recursion

---
+++ 100.same-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        def isSame(t1, t2):
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            return (t1.val == t2.val and isSame(t1.left, t2.left) and
+                    isSame(t1.right, t2.right))
+        
+        return isSame(p, q)
+        
