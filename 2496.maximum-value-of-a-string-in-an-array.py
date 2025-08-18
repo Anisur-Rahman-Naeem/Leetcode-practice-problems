494d115b560daba2d7bec78ddb8b385dc78da01d
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-08-18 19:17:19 +0600

    problem 2496 solved

---
+++ 2496.maximum-value-of-a-string-in-an-array.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=2496 lang=python
+#
+# [2496] Maximum Value of a String in an Array
+#
+
+# @lc code=start
+class Solution(object):
+    def maximumValue(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: int
+        """
+        max_value = 0
+        for word in strs:
+            if word.isdigit():
+
+                if int(word) > max_value:
+                    max_value = int(word)
+            else:
+
+                if len(word) > max_value:
+                    max_value = len(word)
+        
+        return max_value
+
+# @lc code=end
+
