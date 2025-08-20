36ccccfde242d523a1851b9bf383b8fbb41d971e
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-08-21 00:22:46 +0600

    solved using ord()

---
+++ 3110.score-of-a-string.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=3110 lang=python
+#
+# [3110] Score of a String
+#
+
+# @lc code=start
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        score = 0
+        for i in range(1,len(s)):
+            score += abs(ord(s[i]) - ord(s[i - 1]))
+        return score
+# @lc code=end
+
