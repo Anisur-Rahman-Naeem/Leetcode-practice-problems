25e75b6fe5a4e063ca22627cf57aff744a5aed1e
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-09-12 14:44:02 +0600

    used bin function and count for frequency of 1

---
+++ 191.number-of-1-bits.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=191 lang=python
+#
+# [191] Number of 1 Bits
+#
+
+# @lc code=start
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        bin_convert = bin(n)[2:]
+        return bin_convert.count("1")
+# @lc code=end
+
