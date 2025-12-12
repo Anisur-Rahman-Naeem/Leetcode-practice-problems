50d2d8d8f105e68a035aafd3bec088feabf678bf
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-12-12 23:50:47 +0600

    binary to int using bitwise operator

---
+++ 1290.convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=1290 lang=cpp
+ *
+ * [1290] Convert Binary Number in a Linked List to Integer
+ */
+
+// @lc code=start
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    int getDecimalValue(ListNode *head)
+    {
+        ListNode current = head;
+        int result = 0
+        while (current!=nullptr)
+        {
+            result = (result << 1) | current->val;
+            current = current->next;
+        }
+        return result;
+    }
+};
+// @lc code=end
