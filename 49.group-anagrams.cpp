b843b57183ae29001ac123ccd83e8b26ee43996f
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2026-03-04 12:37:10 +0600

    Group strings into anagrams using sorted keys and hashmap

---
+++ 49.group-anagrams.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=49 lang=cpp
+ *
+ * [49] Group Anagrams
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groupAnagrams;
+        vector<vector<string>> vectorOfAnagrams;
+
+        for (int i = 0; i < strs.size(); i++)
+        {
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            groupAnagrams[temp].push_back(strs[i]);
+        }
+        for (auto pair : groupAnagrams)
+        {
+            vectorOfAnagrams.push_back(pair.second);
+        }
+        
+        return vectorOfAnagrams;
+    }
+};
+// @lc code=end
+
