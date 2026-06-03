30a5a2cd890830e7cd33caabd2909d0b9cde5efd
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2026-06-03 12:02:50 +0600

    solved multi test cases problem

---
+++ atcoder/Multi_Test_Cases.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, n, a, count = 0;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n;
+        count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a;
+            if (a % 2 != 0)
+            {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+}
