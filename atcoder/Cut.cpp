39da9ace6054a2af1dce466fb559a84b90439ffe
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2026-06-05 12:30:01 +0600

    Did solution for the Cut problem from AtCoder Beginner Contest 368.

---
+++ atcoder/Cut.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int arr[n] = {};
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = n - k; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    for (int i = 0; i < n - k; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
