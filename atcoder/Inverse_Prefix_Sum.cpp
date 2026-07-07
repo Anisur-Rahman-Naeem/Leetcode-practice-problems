4e66273a967636d2b263c0bee228350e2b0f8eb9
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2026-07-07 12:08:57 +0600

    Solve AtCoder ABC B - Inverse Prefix Sum using prefix difference approach

---
+++ atcoder/Inverse_Prefix_Sum.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n] = {};
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << arr[0] << " ";
+
+    if (n > 1)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            cout << arr[i] - arr[i - 1] << " ";
+        }
+    }
+}
