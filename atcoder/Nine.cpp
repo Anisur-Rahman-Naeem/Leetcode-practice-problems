5ea122abe9d6d41c5230f55ade4df7fa84b2bf6c
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2026-07-11 16:59:51 +0600

    feat: solve AtCoder A - Nine using horizontal adjacency checks

---
+++ atcoder/Nine.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (((a == 1) && (b == 2)) || ((a == 2) && (b == 3)) || ((a == 4) && (b == 5)) || ((a == 5) && (b == 6)) || ((a == 7) && (b == 8)) || ((a == 8) && (b == 9)))
+    {
+        cout<<"Yes"<<endl;
+    }else
+    {
+        cout<<"No"<<endl;
+    }
+    
+}
