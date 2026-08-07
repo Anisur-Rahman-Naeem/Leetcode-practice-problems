664140b5f27af89871a4c85edec5f80f32b7043e
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2026-08-08 03:20:31 +0600

    solved Unnatural Language Processing

---
+++ codeforces/D_Unnatural_Language_Processing.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        char s[n + 1];
+        scanf("%s", s);
+
+        char s2[n + 1];
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == 'a' || s[i] == 'e')
+            {
+                s2[i] = 'V';
+            }
+            else
+            {
+                s2[i] = 'C';
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i+2==n)
+            {
+                printf("%c%c",s[i],s[i+1]);
+                break;
+            }
+            else if (i+2 == n-1)
+            {
+                printf("%c%c%c",s[i],s[i+1],s[i+2]);
+                break;
+            }
+            if (s2[i+3] == 'V')
+            {
+                printf("%c%c.",s[i],s[i+1]);
+                i++;
+            }else
+            {
+                printf("%c%c%c.",s[i],s[i+1],s[i+2]);
+                i+=2;
+            }
+        }
+        printf("\n");
+    }
+}
